4b6ac11e01de8200cb48aee2d6267cd78d77461e
Jean Michel <jean.michel@imj-prg.fr>
2026-01-13 16:42:05 +0100

    made coefficients an OrderedDict

--- src/PuiseuxPolynomials.jl
+++ src/PuiseuxPolynomials.jl
@@ -334,7 +334,7 @@ shabby.  For the Fateman test f(f+1)  where f=(1+x+y+z+t)^15, we take 3sec.
 According to the Nemo paper, Sagemath takes 10sec and Nemo takes 1.6sec.
 """
 module PuiseuxPolynomials
-using ModuleElts, Reexport
+using ModuleElts, Reexport, OrderedCollections
 using LinearAlgebra:LinearAlgebra, exactdiv
 @reexport using LaurentPolynomials
 export coefficient, monomials, powers
@@ -819,34 +819,33 @@ LaurentPolynomials.coefficients(p::Mvp)=values(p.d)
 """
   `coefficients(p::Mvp, var::Symbol)` 
 
-returns  a Dict with keys the degree  in `var` and values the corresponding
-coefficient of `p` with respect to `var`.
+returns  an  OrderedDict  with  keys  the  degree  in  `var` and values the
+corresponding coefficient of `p` with respect to `var`.
 
 ```julia-repl
 julia> p=(x+y+inv(y))^4
 Mvp{Int64}: x⁴+4x³y+4x³y⁻¹+6x²y²+12x²+6x²y⁻²+4xy³+12xy+12xy⁻¹+4xy⁻³+y⁴+4y²+6+4y⁻²+y⁻⁴
 
 julia> coefficients(p,:x)
-Dict{Int64, Mvp{Int64, Int64}} with 5 entries:
+OrderedCollections.OrderedDict{Int64, Mvp{Int64, Int64}} with 5 entries:
   0 => y⁴+4y²+6+4y⁻²+y⁻⁴
-  4 => 1
+  1 => 4y³+12y+12y⁻¹+4y⁻³
   2 => 6y²+12+6y⁻²
   3 => 4y+4y⁻¹
-  1 => 4y³+12y+12y⁻¹+4y⁻³
+  4 => 1
 
 julia> coefficients(p,:y)
-Dict{Int64, Mvp{Int64, Int64}} with 9 entries:
-  0  => x⁴+12x²+6
-  4  => 1
-  -1 => 4x³+12x
-  2  => 6x²+4
+OrderedCollections.OrderedDict{Int64, Mvp{Int64, Int64}} with 9 entries:
+  -4 => 1
   -3 => 4x
   -2 => 6x²+4
-  -4 => 1
-  3  => 4x
+  -1 => 4x³+12x
+  0  => x⁴+12x²+6
   1  => 4x³+12x
+  2  => 6x²+4
+  3  => 4x
+  4  => 1
 ```
-
 The  same  caveat  is  applicable  to  `coefficients` as to evaluating: the
 values  are always `Mvp`s. To get a list of scalars for the coefficients of
 a  univariate polynomial represented as a `Mvp`, one should use `scalar` on
@@ -865,7 +864,8 @@ function LaurentPolynomials.coefficients(p::Mvp{T,N},v::Symbol)where {T,N}
     end
     if !found  d[0]=push!(get!(d,0,empty(pairs(p))),m=>c) end
   end
-  Dict(dg=>Mvp_(c) for (dg,c) in d) # c is sorted by defn of monomial order
+  # c is sorted by defn of monomial order
+  OrderedDict(dg=>Mvp_(c) for (dg,c) in sort(OrderedDict(d),by=first)) 
 end
 
 """
